22e94d100caff5ec4664d4eafb9e0e9a70c8b618
Deepesh <deepesh.kt10636@gmail.com>
2020-07-04 20:24:24 +0530

    React form Validation

--- reactform/src/app/app.component.ts
+++ reactform/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +13,42 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.user = new FormGroup({
-      firstName : new FormControl(""),
-      lastName : new FormControl(""),
+      firstName : new FormControl("",[Validators.minLength(3),Validators.required]),
+      lastName : new FormControl("",[Validators.minLength(3)]),
+      emailId : new FormControl("",Validators.email)
     });
+  }
+
+  get isFirstNameInvalid(){
+    return this.user.touched && !(this.user.controls['firstName'].errors===null);
+  }
+
+  get isLastNameInvalid(){
+    return this.user.touched && !(this.user.controls['lastName'].errors===null);
+  }
+
+  get isEmailInvalid(){
+    return this.user.touched && !(this.user.controls['email'].errors===null);
+  }
+
+
+  get firstNameValidationMessage(){
+    const errors = this.user.controls['firstName'].errors;
+    
+    if(errors['required']){
+      return "First Name Required";
+    }
+    if(errors['minlength']){
+      const minLengthError = errors['minlength'];
+      return `First Name should be minimum of ${minLengthError.requiredLength}character`;
+    }
+  }
+  get lastNameValidationMessage(){
+    const errors = this.user.controls['lastName'].errors;
+       
+    if(errors['minlength']){
+      const minLengthError = errors['minlength'];
+      return `Last Name should be minimum of ${minLengthError.requiredLength}character`;
+    }
   }
 }
